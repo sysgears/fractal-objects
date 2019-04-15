08040bc2709dc07b9330fa950e4464ee1ecef963
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-04-15 11:52:58 +0300

    Add initial docs

--- typescript/README.md
+++ typescript/README.md
@@ -11,16 +11,42 @@ npm install --save-dev fractal-objects
 
 ## Usage
 ``` js
-import { unfold } from 'fractal-objects';
+import { fold } from 'fractal-objects';
 
 const part1 = { arrayKey: [1], objectKey: { a: 'a', b: 'b' } };
 const part2 = { arrayKey: [2, 3], objectKey: { c: 'c' } };
 const part3 = { arrayKey: [4] }
 
-console.log(unfold([part1, part2, part3]));
+console.log(fold([part1, part2, part3]));
 // Output:
 // { arrayKey: [ 1, 2, 3, 4 ], objectKey: { a: 'a', b: 'b', c: 'c' } }
 ```
+
+## Concept
+A fractal object is an object that has self-similarity when
+being multiplied to another fractal object. The result of such
+multiplication is a fractal object having the same shape.
+
+If we run multiplication on a list of fractal objects, by
+multiplying items in a list pairwise we will receive
+fractal object again which will be the representation
+of all the fractal objects in the list and which will have
+the same form.
+
+By default multiplication function provided by this package
+multiplies two objects by concatenating array values,
+merging object values and replacing scalars with value
+from the second multipled object. But any other multiplication
+function can be used provided it has the following properties:
+  1. It keeps the shape of the object. E.g multiplicands and     result have the same shape and the same type.
+  2. It is commutative - changing the order of multiplication
+    should not change result.
+  3. It is associative - e.g. multiplying (a b) c should have
+    the same result as multiplying a (b c)
+  4. Multiplication of `undefined a` or `a undefined` yields     `a`
+
+In terms of mathematics the fractal objects is a commutative
+semigroup.
 
 ## License
 Copyright © 2018 [SysGears (Cyprus) Limited]. This source code is licensed under the [MIT] license.
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,57 @@
+/**
+ * @file Provides basic functions to fold and unfold fractal objects.
+ *
+ * A fractal object is an object that has self-similarity when
+ * being multiplied to another fractal object. The result of such
+ * multiplication is a fractal object having the same shape.
+ *
+ * If we run multiplication on a list of fractal objects, by
+ * multiplying items in a list pairwise we will receive
+ * fractal object again which will be the representation
+ * of all the fractal objects in the list and which will have
+ * the same form.
+ *
+ * By default multiplication function provided by this package
+ * multiplies two objects by concatenating array values,
+ * merging object values and replacing scalars with value
+ * from the second multipled object. But any other multiplication
+ * function can be used provided it has the following properties:
+ *   1. It keeps the shape of the object. E.g multiplicands and result
+ *      have the same shape and the same type.
+ *   2. It is commutative - changing the order of multiplication
+ *      should not change result.
+ *   3. It is associative - e.g. multiplying (a * b) * c should have
+ *      the same result as multiplying a * (b * c)
+ *   4. Multiplication of `undefined * a` or `a * undefined` yields `a`
+ *
+ * In terms of mathematics the fractal objects is a commutative
+ * semigroup.
+ */
 const PARTS_KEY = '__parts';
 
-const multiply = <P>(p1: P, p2: P, options: MultiplyOptions): P => {
+/**
+ * Multiplies two objects by concatenating array values,
+ * merging object values and replacing scalars with value
+ * from the second multipled object.
+ *
+ * Both two arguments and the result of multiplication must
+ * have the same generic type.
+ *
+ * Example:
+ * ```
+ * const p1 = { arrayKey: [1], objectKey: { a: 'a', b: 'b' } };
+ * const p2 = { arrayKey: [2, 3], objectKey: { c: 'c' } };
+ * console.log(defaultMultiply(p1, p2));
+ * // Output:
+ * { arrayKey: [1, 2, 3], objectKey: { a: 'a', b: 'b', c: 'c' } }
+ * ```
+ *
+ * @param p1 first multiplied object
+ * @param p2 second multiplied object
+ *
+ * @returns multiplication result
+ */
+export const defaultMultiply = <P>(p1: P, p2: P): P => {
   if (!p1 && !p2) {
     return undefined;
   }
@@ -59,26 +110,63 @@ const multiply = <P>(p1: P, p2: P, options: MultiplyOptions): P => {
   return result;
 };
 
-export interface MultiplyOptions {
-  resultName?: string;
+/**
+ * Fold function options
+ */
+export interface FoldOptions {
+  /**
+   * A function to be used by `fold` that multiplies two
+   * objects of the same generic type `<P>` and returns
+   * the result of type `<P>`. The `fold` given the object list
+   * as argument runs multiplication function to
+   * multiply all the item in the list into single result.
+   *
+   * For example, given the list `[a, b, c]` the fold function
+   * will yield `result = a * b * c`, where `*` is a
+   * multiplication function.
+   *
+   * Multiplication function must fulfil the following requirements:
+   *   1. It keeps the shape of the object. E.g multiplicands and result
+   *      have the same shape and the same type.
+   *   2. It is commutative - changing the order of multiplication
+   *      should not change result.
+   *   3. It is associative - e.g. multiplying (a * b) * c should have
+   *      the same result as multiplying a * (b * c)
+   *   4. Multiplication of `undefined * a` or `a * undefined` yields `a`
+   *
+   * @param p1 first multiplied object
+   * @param p2 second multiplied object
+   *
+   * @returns multiplication result
+   */
+  multiply?<P>(part1: P, part2: P): P;
 }
 
-export interface FoldOptions extends MultiplyOptions {
-  multiply?<P>(part1: P, part2: P, foldOptions?: MultiplyOptions): P;
-}
-
+/**
+ * A function that given the object list of the same generic
+ * type `<P>` yields the single result of type `<P>` by
+ * running multiplication function.
+ *
+ * For example, given the list `[a, b, c]` the fold function
+ * will yield `result = a * b * c`, where `*` is a
+ * multiplication function.
+ *
+ * @param parts an object list to be multiplied together
+ * @param options fold options
+ */
 export const fold = <P>(parts: P[], options?: FoldOptions): P => {
-  const defOpts = options || {};
-  const opts: MultiplyOptions = { resultName: defOpts.resultName };
+  const opts = {
+    multiply: options.multiply || defaultMultiply
+  };
   let fractal;
   if (!parts || parts.length === 0) {
     fractal = undefined;
   } else if (parts.length === 1) {
-    fractal = multiply(parts[0], undefined, opts);
+    fractal = opts.multiply(parts[0], undefined);
   } else {
     fractal = parts[0];
     for (let idx = 1; idx < parts.length; idx++) {
-      fractal = multiply(fractal, parts[idx], opts);
+      fractal = opts.multiply(fractal, parts[idx]);
     }
   }
   if (fractal) {
@@ -99,10 +187,22 @@ export const fold = <P>(parts: P[], options?: FoldOptions): P => {
   return fractal;
 };
 
+/**
+ * A function that given the object list of the same generic
+ * type `<P>` yields the single result of type `<P>` by
+ * running multiplication function and stores the result
+ * into `whole` parameter
+ *
+ * For example, given the list `[a, b, c]` the fold function
+ * will yield `result = a * b * c`, where `*` is a
+ * multiplication function.
+ *
+ * @param whole the destination object, to store multiplication result to
+ * @param parts an object list to be multiplied together
+ * @param options fold options
+ */
 export const foldTo = <P, W extends P>(whole: W, parts: P[], options?: FoldOptions): void => {
-  const defOpts = options || {};
-  const opts: FoldOptions = { multiply: defOpts.multiply || multiply };
-  const result = fold(parts, opts);
+  const result = fold(parts, options);
   if (result) {
     for (const key of Object.keys(result)) {
       whole[key] = result[key];
@@ -111,6 +211,14 @@ export const foldTo = <P, W extends P>(whole: W, parts: P[], options?: FoldOptio
   }
 };
 
+/**
+ * Given multiplication result produced by `fold` or `foldTo`
+ * returns the object list that were multiplied together.
+ *
+ * @param whole a multiplication result produced by `fold` or `foldTo`
+ *
+ * @returns the object list that were multiplied together
+ */
 export const getParts = <P, W extends P>(whole: W): P[] => {
   return whole && (whole as any)[PARTS_KEY] ? (whole as any)[PARTS_KEY] : [];
 };
